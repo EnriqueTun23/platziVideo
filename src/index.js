ed3baede233683c1b11918f69acbe2555642a402
Enrique Tun Xix <quiquejesus94@gmail.com>
2020-05-23 13:35:05 -0500

    Inicializando proyecto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+// components
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById('app'));
+
