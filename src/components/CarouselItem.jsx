9661c754538f8352745af1e037ed8a9be7638e91
Enrique Tun <quiquejesus94@gmail.com>
2020-06-23 22:47:26 -0500

    action para ejecutar el playing y el componente del vide

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 // actions
 import { setFavorite, deleteFavorite } from '../actions';
 // styles
@@ -26,7 +27,14 @@ const CarouselItem = (props) => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
+          <Link to={`/player/${id}`}>
+            <img
+              className='carousel-item__details--img'
+              src={playIcon}
+              alt='Play Icon'
+            />
+          </Link>
+
           {/* operacion ternaria */}
           {
             isList ? (
